c8e3658eca83d34fdf0631d4f1dc0d1449db7f7b
Innis Macleod <40601397+JorgonSkylisk@users.noreply.github.com>
2018-09-12 15:59:41 +0100

    Skeleton of program

--- Main.cpp
+++ Main.cpp
@@ -1,22 +1,62 @@
 #include <SFML/Graphics.hpp>
+// Library needed for playing music and sound effects
+#include <SFML/Audio.hpp>
+#include <string>
+
+
+
 int main()
 {
-	sf::RenderWindow window(sf::VideoMode(200, 200), "SFML works!");
-	sf::CircleShape shape(100.f);
-	shape.setFillColor(sf::Color::Green);
+	//**************************
+	//********game setup********
+	//**************************	
 
-	while (window.isOpen())
+	// declare variable for display window
+	sf::RenderWindow gamewindow;
+	gamewindow.create(sf::VideoMode::getDesktopMode(), "Quick Draw",
+		sf::Style::Titlebar | sf::Style::Close);
+
+
+
+
+	//*************************
+	//******* game loop *******
+	//*************************
+	while (gamewindow.isOpen())
 	{
-		sf::Event event;
-		while (window.pollEvent(event))
+	
+		// *** Input ***
+		// TODO: Check for input
+		sf::Event gameEvent;
+		while (gamewindow.pollEvent(gameEvent))
 		{
-			if (event.type == sf::Event::Closed)
-				window.close();
+			// Process events
+
+			if (gameEvent.type == sf::Event::MouseButtonPressed)
+			{
+			
+			}
+
+
+			//check if event is closed
+			if (gameEvent.type == sf::Event::Closed)
+			{
+				//close game window
+				gamewindow.close();
+			}
 		}
-		window.clear();
-		window.draw(shape);
-		window.display();
-	}
+
+		// *** Draw Graphics ***
+		gamewindow.clear(sf::Color::Transparent);
+		
+		// *** draw everything ***
+
+
+		//display window content
+		gamewindow.display();
+
+	}// end of game loop
+
 
 	return 0;
-}
+}
